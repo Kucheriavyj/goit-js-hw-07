16451551f3c228d81f1b66f4dff23d149e1c2a34
Kucheriavyj <98981192+Kucheriavyj@users.noreply.github.com>
2023-07-12 07:56:58 +0300

    Добавляет ссылки и скрипты:
    1)Добавляет ссылки на скрипты библиотеке "basicLightbox" в 01-gallery.html;
    2) Добавляет ссылки на скрипты библиотеке "SimpleLightbox" в 02-gallery.html;
    3) Пишет скрипт для первого задания в 01-lightbox.js;
    4) Пишет скрипт для второго задания в 02-lightbox.js;

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  divGalleryEl: document.querySelector(".gallery"),
+};
+
+refs.divGalleryEl.append(...createGallery(galleryItems));
+
+refs.divGalleryEl.addEventListener("click", onOpenModalImage);
+
+function onOpenModalImage(event) {
+  if (event.target.nodeName !== "IMG") return;
+
+  event.preventDefault();
+
+  const instance = basicLightbox.create(`
+    <img src=${event.target.dataset.source} width=1280>
+`);
+
+  instance.show();
+
+  document.addEventListener("keydown", (event) => {
+    if (event.code !== "Escape") return;
+    instance.close();
+  });
+}
+
+function createGallery(arrOfGallaryItems) {
+  return arrOfGallaryItems.map((galleryItem) => {
+    const galleryItemEl = document.createElement("div");
+    const galleryLinkEl = document.createElement("a");
+    const galleryImageEl = document.createElement("img");
+
+    galleryImageEl.classList.add("gallery__image");
+    galleryImageEl.setAttribute("src", galleryItem.preview);
+    galleryImageEl.setAttribute("alt", galleryItem.description);
+    galleryImageEl.dataset.source = galleryItem.original;
+
+    galleryLinkEl.classList.add("gallery__link");
+    galleryLinkEl.setAttribute("href", galleryItem.original);
+
+    galleryItemEl.classList.add("gallery__item");
+
+    galleryLinkEl.append(galleryImageEl);
+    galleryItemEl.append(galleryLinkEl);
+
+    return galleryItemEl;
+  });
+}
